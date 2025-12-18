69e9ce37565c15f35c69994c242f06da13db89f7
olgararo <olgararo1402@gmail.com>
2025-12-18 16:06:29 +0100

    feat: configure cors and dotenv

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-/*(1.) Archivo principal cuyo trabajo es arrancar y mantenerse a la espera de peticiones
-
-- Utilizo la library express para poder crear el servidor
-- Creo una instancia de la aplocación express
-- Creo una primera ruta de prueba
-- Defino el puerto en el que el servidor va a escuchar peticiones
--Arranco el servidor para que empiece a escuchar el puerto definido
-
-(2.3) Decirle a la aplicación principal que use el nuevo routes
-*/
+//(1.) Archivo principal cuyo trabajo es arrancar y mantenerse a la espera de peticiones
 
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import predictionRoutes from "./routes/predictionRoutes.js";
 
+dotenv.config();
+
 export const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 // Ruta raíz
 app.get("/", (req, res) => {
-  res.send("Hola, API de Madame Web. Estoy viiiiiiva.... viiiiiiva!");
+  res.send("Hola, soy la API de Madame Web. Estoy viiiiiiva.... viiiiiiva!");
 });
 
 //Rutas de la API
 app.use("/api", predictionRoutes);
-
-const PORT = 3001;
 
 export const server = app.listen(PORT, () => {
   console.log(
